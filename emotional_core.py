06827d781353cb30d9b0952bfb8376a32241597a
Unknown <rob.marshall06@gmail.com>
2018-09-28 11:05:28 -0400

    added emotional_levels and responses

--- emotional_core.py
+++ emotional_core.py
@@ -9,11 +9,34 @@ class Emotions:
 
     @commands.command(pass_context=True)
     async def status(self, ctx):
-        responses = ["Artemis is ok!",
-                     "Artemis is currently online.",
-                     ":thumbsup:",
-                     "No problems, at the moment."]
-        await self.client.send_message(ctx.message.channel, random.choice(responses))
+        emotional_level = 50
+        great = ["Artemis is doing great!",
+                 "Everything is optimal.",
+                 "Everything looks good from here!",
+                 ":sparkler:"]
+        good = ["Artemis is good!",
+                "No problems, at the moment.",
+                ":thumbsup:"]
+        ok = ["Artemis is ok!",
+              "Everything is fine.",
+              "Nothing to report."]
+        bad = ["Artemis is doing poorly",
+               "Eh, things could be better.",
+               "I don't feel so good, Mr. Star-"]
+        terrible = ["Artemis is doing terribly",
+                    "Couldn't get any worse in here.",
+                    "Artemis is operating at sub-optimal levels.",
+                    "Everything is on fire!"]
+        if emotional_level > 80:
+            await self.client.send_message(ctx.message.channel, random.choice(great))
+        elif emotional_level > 60:
+            await self.client.send_message(ctx.message.channel, random.choice(good))
+        elif emotional_level > 40:
+            await self.client.send_message(ctx.message.channel, random.choice(ok))
+        elif emotional_level > 20:
+            await self.client.send_message(ctx.message.channel, random.choice(bad))
+        else:
+            await self.client.send_message(ctx.message.channel, random.choice(terrible))
 
 
 def setup(client):
