d4e8dff7cf59acf1024f60553d8fd1ade7e16b9e
Hatchet Jackk <rob.marshall06@gmail.com>
2018-10-10 18:17:26 -0400

    events update

--- cogs/events.py
+++ cogs/events.py
@@ -19,9 +19,9 @@ class Events:
         # every five minutes.
         # todo add 'hours from now'
         now = datetime.datetime.now
-        zones = {'est': now(pytz.timezone('US/Eastern')), 'edt': now(pytz.timezone('US/Eastern')),
+        zones = {'pst': now(pytz.timezone('US/Alaska')), 'pdt': now(pytz.timezone('US/Alaska')),
                  'cst': now(pytz.timezone('US/Mountain')), 'cdt': now(pytz.timezone('US/Mountain')),
-                 'pst': now(pytz.timezone('US/Alaska')), 'pdt': now(pytz.timezone('US/Alaska')),
+                 'est': now(pytz.timezone('US/Eastern')), 'edt': now(pytz.timezone('US/Eastern')),
                  'gmt': now(pytz.timezone('GMT')), 'bst': now(pytz.timezone('Europe/London')),
                  'utc': now(pytz.timezone('UTC'))}
 
@@ -47,22 +47,27 @@ class Events:
             return
 
     @commands.command(pass_context=True)
-    async def time(self, ctx, arg):
+    async def time(self, ctx):
         now = datetime.datetime.now
-        zones = {'est': now(pytz.timezone('US/Eastern')), 'edt': now(pytz.timezone('US/Eastern')),
-                 'cst': now(pytz.timezone('US/Mountain')), 'cdt': now(pytz.timezone('US/Mountain')),
-                 'pst': now(pytz.timezone('US/Alaska')), 'pdt': now(pytz.timezone('US/Alaska')),
-                 'gmt': now(pytz.timezone('GMT')), 'bst': now(pytz.timezone('Europe/London')),
-                 'utc': now(pytz.timezone('UTC'))}
-        if arg not in zones:
-            await self.client.send_message(ctx.message.channel, '{0} is not a valid timezone.'.format(arg.upper()))
-            return
-        if arg == 'utc':
-            await self.client.send_message(ctx.message.channel,
-                                           'The current time for {0} is {1}.'.format(arg.upper(), zones[arg].strftime('%H:%M')))
-            return
-        await self.client.send_message(ctx.message.channel,
-                                       'The current time for {0} is {1}. (UTC time is {2}) '.format(arg.upper(), zones[arg].strftime('%H:%M'), zones['utc'].strftime('%H:%M')))
+        zones = {'pst/pdt': now(pytz.timezone('US/Alaska')),
+                 'cst/cdt': now(pytz.timezone('US/Mountain')),
+                 'est/edt': now(pytz.timezone('US/Eastern')),
+                 'utc/gmt': now(pytz.timezone('GMT')),
+                 'bst': now(pytz.timezone('Europe/London'))}
+
+        embed = discord.Embed(
+            title='──────────────── [ Time ] ────────────────',
+            color=discord.Color.blue())
+        embed.add_field(
+            name='Zone',
+            value='\n'.join([zone.upper() for zone in zones]),
+            inline=True)
+        embed.add_field(
+            name='Time',
+            value='\n'.join(zones[zone].strftime('%H:%M') for zone in zones))
+        embed.set_footer(
+            text='──────────────────────────────────────────')
+        await self.client.send_message(ctx.message.channel, embed=embed)
 
     @commands.command(pass_context=True)
     async def delevent(self, ctx, *args):
@@ -116,9 +121,10 @@ class Events:
         await self.client.send_message(ctx.message.channel, embed=embed)
 
     @commands.command(pass_context=True)
-    async def checkevents(self, ctx, *args):
+    async def events(self, ctx, *args):
         # check current events that are active from current time into the future
-        thumb_url = 'https://images-ext-1.discordapp.net/external/veD-zTXyh96Zn-MB2t3vXqiZrRlihx4r5DCnrJ0nEh0/https/i.imgur.com/JK61b19.png?width=676&height=676'
+        thumb_url = 'https://images-ext-1.discordapp.net/external/veD-zTXyh96Zn-MB2t3vXqiZrRlihx4r5DCnrJ0nEh0/https/' \
+                    'i.imgur.com/JK61b19.png?width=676&height=676'
         event_id = 0
         try:
             event_id = int(args[0])
@@ -133,9 +139,9 @@ class Events:
 
         with open('files/events.json') as f:
             data = json.load(f)
-        # return single event
-        event_id = str(event_id)
-        if event_id in data:
+
+        # if an event id is passed, attempt to return that event
+        if str(event_id) in data:
             event = data[event_id]['event']
             time = data[event_id]['time']
             zone = data[event_id]['zone'].upper()
@@ -154,24 +160,42 @@ class Events:
                             value='placeholder')
             embed.set_footer(text='──────────────────────────────────────────')
             await self.client.send_message(ctx.message.channel, embed=embed)
+
+        # if no arguments are passed or event_id is not in data, return all events
         else:
+            # todo allow the user to page through other events
             embed = discord.Embed(
                 title='──────────────── [Events] ────────────────',
                 color=discord.Color.blue()
             )
             embed.set_thumbnail(url=thumb_url)
-            for key, value in data.items():
-                embed.add_field(name=value['event'],
-                                value='Time: {0} {1} (id: {2})'.format(value['time'], value['zone'].upper(), key),
-                                )
-                embed.add_field(name='ETA',
-                                value='placeholder')
-                embed.set_footer(text='──────────────────────────────────────────')
-            await self.client.send_message(ctx.message.channel, embed=embed)
+            embed.set_footer(text='──────────────────────────────────────────')
 
-    async def event_handler_one(self, time_raw, tz, event, ctx):
+            # iterate through events and truncate event names longer than 50 characters
+            num = 0
+            for event_id, value in data.items():
+                num = num + 1
+                utc = ''
+                if value['zone'].lower() != 'utc':
+                    utc = '| *placeholder* UTC'
+                embed.add_field(name='{0}. {1}'.format(num, value['event'][:50]),
+                                value='Time: {0} {1} {2} | ({3})\nETA {4}'.format(value['time'],
+                                                                                  value['zone'].upper(),
+                                                                                  utc, event_id, '*placeholder*'),
+                                inline=False)
+                if num % 5 == 0:
+                    await self.client.send_message(ctx.message.channel, embed=embed)
+                    break
+
+    async def event_handler_one(self, dt, tz, event, ctx):
+        if type(dt) != datetime.datetime:
+            dt = datetime.datetime.strptime(dt, '%H:%M')
+            dt = dt.strftime('%H:%M')
+        else:
+            dt = dt.strftime('%H:%M')
+
         try:
-            dt = datetime.datetime.strptime(time_raw, '%H:%M')
+            dt = datetime.datetime.strptime(dt, '%H:%M')
             if tz == 'utc':
                 dt2 = await self.utc_handler(dt, tz)
                 tz2 = None
@@ -180,7 +204,7 @@ class Events:
                 tz2 = 'utc'
             dt2 = dt2.strftime('%H:%M')
         except ValueError:
-            await self.client.send_message(ctx.message.channel, '{0} is not a valid time.'.format(time_raw))
+            await self.client.send_message(ctx.message.channel, '{0} is not a valid time.'.format(dt))
             return
 
         with open('files/events.json', 'r') as f:
@@ -200,19 +224,19 @@ class Events:
 
     @staticmethod
     async def event_handler_two(ctx, args):
-        # handle input for hours and minutes
-        print(args)
-        utc = datetime.datetime.now(pytz.timezone('UTC'))
-        print(utc)
-        time = args[0]
-        print(time)
-        time = time.split('h')
-        print(time)
-        hours = time[0]
-        minutes = time[1].strip('m')
-        print(hours, minutes)
-        zone = args[1]
-        event = ' '.join(args[2:])
+        # # handle input for hours and minutes
+        # print(args)
+        # utc = datetime.datetime.now(pytz.timezone('UTC'))
+        # print(utc)
+        # time = args[0]
+        # print(time)
+        # time = time.split('h')
+        # print(time)
+        # hours = time[0]
+        # minutes = time[1].strip('m')
+        # print(hours, minutes)
+        # zone = args[1]
+        # event = ' '.join(args[2:])
         pass
 
     @staticmethod
