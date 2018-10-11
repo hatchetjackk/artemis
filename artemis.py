41740634c4fbb6cb805137d38a2a7d20c6d8bff3
Hatchet Jackk <rob.marshall06@gmail.com>
2018-10-10 21:17:34 -0400

    updated help

--- artemis.py
+++ artemis.py
@@ -128,30 +128,36 @@ async def on_message(message):
 async def help(ctx, *args):
     if len(args) > 1:
         await client.send_message(ctx.message.channel, 'You\'ve passed too many arguments.')
-    if args[0] == 'events':
-        await events_help(ctx)
-        return
-
+    try:
+        if args[0] == 'events':
+            await events_help(ctx)
+            return
+        else:
+            await client.send_message(ctx.message.channel, '{0} is not an option.'.format(args[0]))
+            return
+    except IndexError:
+        pass
     author = ctx.message.author
     embed = discord.Embed(color=discord.Color.blue())
     embed.set_author(name="Help Page")
     embed.add_field(
         name="How do I give karma?",
-        value="Just say thanks and mention the target\n"
-              "Ex: 'Thanks @Hatchet Jackk'",
-        inline=False)
-    embed.add_field(name="!ping", value="Return pong", inline=False)
-    embed.add_field(name="!roll", value="Roll NdN dice", inline=False)
-    embed.add_field(name="!karma <user>", value="Check your or another <user>'s current level of karma", inline=False)
-    embed.add_field(name='!hello', value='Say hi to Artemis!', inline=False)
-    embed.add_field(name="!status", value="Check Artemis' status", inline=False)
-    embed.add_field(name="!leaderboard", value="Check karma levels (WIP)", inline=False)
-    embed.add_field(name="!arena", value="Settle the score (WIP)", inline=False)
-    embed.add_field(name="!flip", value="Flip a coin", inline=False)
-    embed.add_field(name="!rps <choice>", value="Play Rock, Paper, Scissors against the bot", inline=False)
-    embed.add_field(name="!whois <user>", value="Find user details (WIP)", inline=False)
-    embed.add_field(name="!server", value="Check server information (WIP)", inline=False)
-    embed.add_field(name='!yt <search>', value='Return the first YouTube video based for <search>.', inline=False)
+        value="Just say thanks and mention the target\nEx: 'Thanks @Hatchet Jackk'",
+        inline=False
+    )
+    embed.add_field(name='prefix: {0}'.format(command_prefix), value='\u200b')
+    embed.add_field(name="ping", value="Return pong", inline=False)
+    embed.add_field(name="roll", value="Roll NdN dice", inline=False)
+    embed.add_field(name="karma <user>", value="Check your or another <user>'s current level of karma", inline=False)
+    embed.add_field(name='hello', value='Say hi to Artemis!', inline=False)
+    embed.add_field(name="status", value="Check Artemis' status", inline=False)
+    embed.add_field(name="leaderboard", value="Check karma levels (WIP)", inline=False)
+    embed.add_field(name="arena", value="Settle the score (WIP)", inline=False)
+    embed.add_field(name="flip", value="Flip a coin", inline=False)
+    embed.add_field(name="rps <choice>", value="Play Rock, Paper, Scissors against the bot", inline=False)
+    embed.add_field(name="whois <user>", value="Find user details (WIP)", inline=False)
+    embed.add_field(name="server", value="Check server information (WIP)", inline=False)
+    embed.add_field(name='yt <search>', value='Return the first YouTube video based for <search>.', inline=False)
     embed.add_field(name='help events', value='See available options for events.', inline=False)
     embed.set_footer(text="Author: Hatchet Jackk")
     await client.send_message(author, embed=embed)
