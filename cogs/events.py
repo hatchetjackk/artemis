c99880b8c7affc3c1714a454aa831db4e3d23dd4
Hatchet Jackk <rob.marshall06@gmail.com>
2018-10-12 17:20:37 -0400

    working on using timezones

--- cogs/events.py
+++ cogs/events.py
@@ -21,36 +21,18 @@ class Events:
         # when a user calls !setevent, they can create an event and a time based on specified timezone. only members
         # should be able to do this once every five minutes. The result is sent to an embed and displayed in the channel
         # that it was created in
-
-        now = datetime.datetime.now
-        zones = {'pst': now(pytz.timezone('US/Alaska')), 'pdt': now(pytz.timezone('US/Alaska')),
-                 'cst': now(pytz.timezone('US/Mountain')), 'cdt': now(pytz.timezone('US/Mountain')),
-                 'est': now(pytz.timezone('US/Eastern')), 'edt': now(pytz.timezone('US/Eastern')),
-                 'gmt': now(pytz.timezone('GMT')), 'bst': now(pytz.timezone('Europe/London')),
-                 'utc': now(pytz.timezone('UTC'))}
         if len(args) < 2:
             await self.client.send_message(ctx.message.channel, 'You did not pass enough arguments to create an event.\n'
                                                                 'Use ``setevent <time> <zone> <event>`` to set an event.')
             return
-        dt = args[0]
+        dt = args[0].split(':')
+        h = dt[0]
+        m = dt[1]
         tz = args[1].lower()
         event = ' '.join(args[2:])
-
-        # check for legitimate time zone
-        if tz not in zones:
-            await self.client.send_message(ctx.message.channel,
-                                           '{0} is not a valid timezone.\n'
-                                           'Use ``setevent <time> <zone> <event>`` to set an event.'.format(tz))
-            return
-
+        dt_user_set, utc_conversion = await self.time_handler(ctx, h, m, tz)
         # try/except will check for valid time formats
-        try:
-            dt_d = datetime.datetime.strptime(dt, '%H:%M')
-            await self.event_handler(dt_d, tz, event, ctx)
-        except ValueError as e:
-            print(e)
-            await self.client.send_message(ctx.message.channel, '{0} is not a valid time.'.format(dt))
-            return
+        await self.event_handler(ctx, dt_user_set, utc_conversion, tz, event)
 
     @commands.command(pass_context=True)
     async def time(self, ctx):
@@ -99,7 +81,6 @@ class Events:
 
     @commands.command(pass_context=True)
     async def update(self, ctx, *args):
-        zones = ['est', 'edt', 'cst', 'cdt', 'pst', 'pdt', 'gmt', 'bst', 'utc']
         # update an existing event with a new time and zone
         if len(args) < 1 or len(args) > 3:
             await self.client.send_message(ctx.message.channel, 'Please use the format ``update <event id> <time> <zone>``.')
@@ -107,17 +88,11 @@ class Events:
         # check the validity of the input
         try:
             event_id = args[0]
-            datetime.datetime.strptime(args[1], '%H:%M')
-            time = args[1]
-            if args[2].lower() not in zones:
-                print('Zone not in zones')
-                await self.client.send_message(ctx.message.channel, '{0} is not a valid time zone.'.format(args[2]))
-                return
-            zone = args[2]
-        except ValueError as e:
-            print(e)
-            await self.client.send_message(ctx.message.channel, '{0} is not a valid time.'.format(args[1]))
-            return
+            dt = args[1].split(':')
+            h = dt[0]
+            m = dt[1]
+            tz = args[2].lower()
+            user_set, utc_conv = await self.time_handler(ctx, h, m, tz)
         except IndexError:
             await self.client.send_message(ctx.message.channel,
                                            'Please use the format ``update <event id> <time> <zone>``.')
@@ -125,21 +100,20 @@ class Events:
 
         with open('files/events.json') as f:
             data = json.load(f)
-
         if event_id in data:
             orgnl_dt = data[event_id]['time']
             orgnl_tz = data[event_id]['zone']
-            data[event_id]['time'] = time
-            data[event_id]['zone'] = zone
+            data[event_id]['time'] = user_set
+            data[event_id]['zone'] = tz
         else:
             await self.client.send_message(ctx.message.channel, 'Event ID {0} does not exist.'.format(event_id))
             return
-
         with open('files/events.json', 'w') as f:
             json.dump(data, f, indent=2)
 
         try:
-            embed = await self.embed_handler(data[event_id]['event'], event_id, time, orgnl_dt, zone, orgnl_tz, ctx, method='update')
+            # ctx, event_id, event, dt_user_set, tz, utc_conversion, method)
+            embed = await self.embed_handler(ctx, event_id, data[event_id]['event'], user_set, tz, utc_conv, 'update', orgnl_dt, orgnl_tz)
             await self.client.send_message(ctx.message.channel, embed=embed)
         except Exception as e:
             print(e)
@@ -185,8 +159,8 @@ class Events:
                 name='Time',
                 value='{0} {1} {2}'.format(time, zone, utc)
             )
-            eta = await self.eta(time, zone)
-            embed.add_field(name='ETA', value=eta)
+            # eta = await self.eta(time)
+            # embed.add_field(name='ETA', value=str(eta))
             embed.set_footer(text='──────────────────────────────────────────')
             await self.client.send_message(ctx.message.channel, embed=embed)
             return
@@ -224,22 +198,7 @@ class Events:
                 )
         await self.client.send_message(ctx.message.channel, embed=embed)
 
-    async def event_handler(self, dt, tz, event, ctx):
-        # take in times, zones, and events then add them to events.json
-        try:
-            if type(dt) != datetime.datetime:
-                dt_d = datetime.datetime.strptime(dt, '%H:%M')
-                dt_s = dt.strftime('%H:%M')
-            else:
-                dt_d = dt
-                dt_s = dt.strftime('%H:%M')
-            utc_d, utc_s = await self.time_handler(dt_d, tz)
-        except ValueError:
-            return
-        except AttributeError as e:
-            print(e)
-            return
-
+    async def event_handler(self, ctx, dt_user_set, utc_conversion, tz, event):
         # add event to events.json
         with open('files/events.json', 'r') as f:
             data = json.load(f)
@@ -247,96 +206,101 @@ class Events:
                 event_id = random.randint(1, 99999)
                 if event_id not in data:
                     data[event_id] = {'event': event,
-                                      'time': dt_s,
+                                      'time': str(dt_user_set),
                                       'zone': tz,
                                       'user_id': ctx.message.author.id,
                                       'server_id': ctx.message.server.id}
-                    embed = await self.embed_handler(event, event_id, dt, utc_s, tz, 'UTC', ctx, 'new')
+                    embed = await self.embed_handler(ctx, event_id, event, dt_user_set, tz,
+                                                     utc_conversion, 'new', None, None)
                     await self.client.send_message(ctx.message.channel, embed=embed)
                     break
         with open('files/events.json', 'w') as f:
             json.dump(data, f, indent=2)
 
-    @staticmethod
-    async def time_handler(dt_d, tz):
-        # take a time and converts to utc datetime
-        tz = tz.lower()
-        tdelta = datetime.timedelta
-        zones = {'est': 4, 'edt': 4,
-                 'cst': 5, 'cdt': 5,
-                 'pst': 7, 'pdt': 7,
-                 'gmt': 0, 'bst': -1,
-                 'utc': 0}
-        if tz in zones:
-            if type(dt_d) != datetime.datetime:
-                try:
-                    dt_d = datetime.datetime.strptime(dt_d, '%H:%M')
-                    utc_d = dt_d + tdelta(hours=zones[tz])
-                    utc_s = utc_d.strftime('%H:%M')
-                    return utc_d, utc_s
-                except ValueError as e:
-                    print(e)
-                    return
-            if type(dt_d) == datetime.datetime:
-                try:
-                    utc_d = dt_d + tdelta(hours=zones[tz])
-                    utc_s = utc_d.strftime('%H:%M')
-                    return utc_d, utc_s
-                except ValueError as e:
-                    print(e)
-                    return
+    async def time_handler(self, ctx, h, m, tz):
+        d = datetime.datetime.now
+        try:
+            h = int(h)
+            m = int(m)
+            zones = {'pst': (pytz.timezone('US/Alaska')), 'pdt': (pytz.timezone('US/Alaska')),
+                     'cst': (pytz.timezone('US/Mountain')), 'cdt': (pytz.timezone('US/Mountain')),
+                     'est': (pytz.timezone('US/Eastern')), 'edt': (pytz.timezone('US/Eastern')),
+                     'gmt': (pytz.timezone('GMT')), 'bst': (pytz.timezone('Europe/London')),
+                     'utc': (pytz.timezone('UTC'))}
+            # verify timezone
+            if tz not in zones:
+                await self.client.send_message(ctx.message.channel, '{} is not a valid timezone.'.format(tz))
+                return
+            # time set by the user using today's date
+            dt_user_set = datetime.datetime(year=d().year, month=d().month, day=d().day,
+                                            hour=h, minute=m, second=d().second,
+                                            microsecond=d().microsecond, tzinfo=zones[tz])
+            # current time in the tz set by the user
+            dt_current_time = d(zones[tz])
+            # utc time based on time input by the user
+            if zones[tz] == 'utc':
+                utc_conversion = None
+            else:
+                utc_conversion = dt_user_set.astimezone(pytz.timezone('UTC'))
+            # difference between current time and set time
+            diff = dt_user_set - dt_current_time
+            # if the difference is less than 0 days, the set time is considered to be for tomorrow
+            # thus we will add 24 hours to the time.
+            if diff.days < 0:
+                dt_user_set = dt_user_set + datetime.timedelta(hours=24)
+            return dt_user_set, utc_conversion
+        except ValueError as v:
+            print(v)
+            await self.client.send_message(ctx.message.channel, '{0}:{1} is not a valid time.'.format(h, m))
 
-    async def embed_handler(self, event, event_id, dt1, dt2, tz1, tz2, ctx, method):
-        event_title = ':sparkles: Upcoming Event'
-        foot = int((39 - len(ctx.message.author.name)) / 2) * '─'
+    async def embed_handler(self, ctx, event_id, event, dt_user_set, tz, utc_conversion, method, orgnl_dt, orgnl_tz):
         if ctx.message.server.icon_url is '':
             thumb_url = self.client.user.avatar_url
         else:
             thumb_url = ctx.message.server.icon_url
 
+        event_title = ':sparkles: Upcoming Event'
         if method == 'update':
             event_title = ':sparkles: Event Updated'
 
-        # double check time validity
-        if type(dt1) != datetime.datetime:
-            try:
-                dt1 = datetime.datetime.strptime(dt1, '%H:%M')
-                dt1 = dt1.strftime('%H:%M')
-            except ValueError as e:
-                print(e)
-                await self.client.send_message(ctx.message.channel, '{0} is not a valid time.'.format(dt1))
-                return
-        else:
-            dt1 = dt1.strftime('%H:%M')
+        dt_string = datetime.datetime.strftime(dt_user_set, '%H:%M')
+        utc_string = datetime.datetime.strftime(utc_conversion, '%H:%M')
 
         try:
             embed = discord.Embed(title='──────────────── [Events] ────────────────', color=discord.Color.blue())
             embed.set_thumbnail(url=thumb_url)
             embed.add_field(name=event_title, value='**Event** [{0}]: {1}'.format(event_id, event), inline=False)
-            if tz2 is None:
-                embed.add_field(name='Time', value='{0} {1}'.format(dt1, tz1.upper()))
+            if utc_conversion is None:
+                embed.add_field(name='Time', value='{0} {1}'.format(dt_string, tz.upper()))
             else:
                 if method == 'update':
-                    embed.add_field(name='Time', value='{0} {1} -> {2} {3}'.format(dt2, tz2.upper(), dt1, tz1.upper()))
+                    embed.add_field(name='Time', value='{0} {1} -> {2} {3}'.format(orgnl_dt, orgnl_tz.upper(), dt_user_set, tz.upper()))
                 else:
-                    embed.add_field(name='Time', value='{0} {1} | {2} {3}'.format(dt1, tz1.upper(), dt2, tz2.upper()))
-            eta = await self.eta(dt1, tz1)
+                    embed.add_field(name='Time', value='{0} {1} | {2} UTC'.format(dt_string, tz.upper(), utc_string))
+            # todo use event id in eta instead of current set time
+            eta = await self.eta(dt_user_set)
             embed.add_field(name='ETA', value='{0}'.format(eta))
+            foot = int((39 - len(ctx.message.author.name)) / 2) * '─'
             embed.set_footer(text=foot + '[Created by: {0}]'.format(ctx.message.author.name) + foot)
             return embed
         except Exception as e:
             print(e)
 
-    async def eta(self, dt, tz):
-        # calculate the difference between the set time and the current time using UTC
-        utc_d, utc_s = await self.time_handler(dt, tz)
-        utc_now = datetime.datetime.utcnow()
-        utc_now = utc_now.replace(tzinfo=None)
-        diff = utc_d - utc_now
-        hours, remainder = divmod(diff.seconds, 3600)
-        minutes, seconds = divmod(remainder, 60)
-        _eta = '{0}h {1}m'.format(hours, minutes)
-        return _eta
+    async def eta(self, dt_user_set):
+        # # calculate the difference between the set time and the current time using UTC
+        # dt_user_set = dt_user_set.astimezone(pytz.timezone('UTC'))
+        # dt_current_time = datetime.datetime.now(pytz.timezone('UTC'))
+        # # difference between current time and set time
+        # diff = dt_current_time - dt_user_set
+        # if diff.days < 0:
+        #     dt_user_set = dt_user_set + datetime.timedelta(hours=24)
+        # hours, remainder = divmod(diff.seconds, 3600)
+        # minutes, seconds = divmod(remainder, 60)
+        # diff = '{0}h {1}m'.format(hours, minutes)
+        # print(dt_user_set, dt_current_time)
+        # print(diff)
+        # return diff
+        return None
 
 
 def setup(client):
