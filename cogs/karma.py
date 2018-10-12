1b736b23d3e85726a59d7c5a8fc67c756ba8d5fc
Hatchet Jackk <rob.marshall06@gmail.com>
2018-10-12 14:24:38 -0400

    updated karma formatting

--- cogs/karma.py
+++ cogs/karma.py
@@ -15,27 +15,25 @@ class Karma:
     @commands.command(pass_context=True)
     async def karma(self, ctx):
         """ Check karma points """
-        user = ctx.message.author.id
-        target = ctx.message.author
+        author = ctx.message.author
         message = ctx.message.content.split()
-        members = [member for member in ctx.message.server.members]
+        members = ctx.message.server.members
         # check another user's karma
         for member in members:
-            if member.mention in message:
-                target = member.name
-                user = member.id
+            if member.mention in message or member.name.lower() in message:
                 with open('files/users.json', 'r') as f:
                     users = json.load(f)
-                    if user in users:
-                        points = users[user]['karma']
-                        await self.client.send_message(ctx.message.channel, '{0} has {1} karma.'.format(target, points))
+                    if member.id in users:
+                        await self.client.send_message(ctx.message.channel,
+                                                       '{0} has {1} karma.'.format(member.name,
+                                                                                   users[member.id]['karma']))
                         return
-        # check message author's karma
+        # check author's karma
         with open('files/users.json', 'r') as f:
             users = json.load(f)
-            if user in users:
-                points = users[user]['karma']
-                await self.client.send_message(ctx.message.channel, 'You have {1} karma.'.format(target, points))
+            if author.id in users:
+                points = users[author.id]['karma']
+                await self.client.send_message(ctx.message.channel, 'You have {0} karma.'.format(points))
 
     async def generate_karma(self, message):
         """ Generate karma!
@@ -60,36 +58,36 @@ class Karma:
                         "Looks like karma abuse over here.",
                         "Are you trying to farm karma?!"]
 
-        message_word_list = [word.lower() for word in message.content.split()]
+        msg = [word.lower() for word in message.content.split()]
         keywords = ['thanks', 'thank', 'gracias', 'kudos', 'thx', 'appreciate', 'cheers']
-        karma_key = [item for item in keywords if item in message_word_list]
-        user_list = [member for member in message.server.members]
+        karma_key = [item for item in keywords if item in msg]
+        members = message.server.members
         # check that message contains user names and words
-        for user in user_list:
-            if user.mention in message_word_list:
+        for member in members:
+            if member.mention in msg or member.name.lower() in msg:
                 # catch if one or more karma keyword has been passed
                 # this prevents a bug that allows  a user to pass karma multiple times in one post
                 if len(karma_key) > 0:
                     # check if someone is trying to give artemis karma
-                    if user.id is self.client.user.id:
+                    if member.id is self.client.user.id:
                         await self.client.send_message(message.channel, random.choice(client_responses))
                         return
                     # check if someone is trying to give karma for their self
-                    if user.id is message.author.id:
+                    if member.id is message.author.id:
                         await self.client.send_message(message.channel,
                                                        random.choice(bad_response).format(message.author.id))
                         return
                     # if karma is going to a user and not artemis
                     with open('files/users.json', 'r') as f:
-                        users = json.load(f)
-                    await self.add_karma(users, user)
+                        data = json.load(f)
+                    await self.add_karma(data, member)
 
                     with open('files/users.json', 'w') as f:
-                        json.dump(users, f, indent=2)
+                        json.dump(data, f, indent=2)
 
-                    fmt = random.choice(responses).format(user.name)
+                    fmt = random.choice(responses).format(member.name)
                     await self.client.send_message(message.channel, fmt)
-                    print("{0} received a karma point from {1}".format(user.name, message.author.name))
+                    print("{0} received a karma point from {1}".format(member.name, message.author.name))
                     return
 
     @staticmethod
