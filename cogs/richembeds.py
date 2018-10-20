65a8eb1ff83534944517f873e5f7d91409e2d784
Hatchet Jackk <rob.marshall06@gmail.com>
2018-10-20 16:29:10 -0400

    fixed example embed and colors

--- cogs/richembeds.py
+++ cogs/richembeds.py
@@ -7,102 +7,106 @@ from discord.ext import commands
 
 
 class RichEmbed:
+    color_dict = {
+        1: [Color.teal(), 'teal'],
+        2: [Color.dark_teal(), 'dark_teal'],
+        3: [Color.green(), 'green'],
+        4: [Color.dark_green(), 'dark_green'],
+        5: [Color.blue(), 'blue'],
+        6: [Color.dark_blue(), 'dark_blue'],
+        7: [Color.purple(), 'purple'],
+        8: [Color.dark_purple(), 'dark_purple'],
+        9: [Color.magenta(), 'magenta'],
+        10: [Color.dark_magenta(), 'dark_magenta'],
+        11: [Color.gold(), 'gold'],
+        12: [Color.dark_gold(), 'dark_gold'],
+        13: [Color.orange(), 'orange'],
+        14: [Color.dark_orange(), 'dark_orange'],
+        15: [Color.red(), 'red'],
+        16: [Color.dark_red(), 'dark_red'],
+        17: [Color.lighter_grey(), 'lighter_grey'],
+        18: [Color.dark_grey(), 'grey'],
+        19: [Color.light_grey(), 'light_grey'],
+        20: [Color.darker_grey(), 'darker_grey']
+    }
+
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
-    async def colors(self, ctx, *args):
-        colors = {
-            1: [Color.teal(), 'teal'],
-            2: [Color.dark_teal(), 'dark_teal'],
-            3: [Color.green(), 'green'],
-            4: [Color.dark_green(), 'dark_green'],
-            5: [Color.blue(), 'blue'],
-            6: [Color.dark_blue(), 'dark_blue'],
-            7: [Color.purple(), 'purple'],
-            8: [Color.dark_purple(), 'dark_purple'],
-            9: [Color.magenta(), 'magenta'],
-            10: [Color.dark_magenta(), 'dark_magenta'],
-            11: [Color.gold(), 'gold'],
-            12: [Color.dark_gold(), 'dark_gold'],
-            13: [Color.orange(), 'orange'],
-            14: [Color.dark_orange(), 'dark_orange'],
-            15: [Color.red(), 'red'],
-            16: [Color.dark_red(), 'dark_red'],
-            17: [Color.lighter_grey(), 'lighter_grey'],
-            18: [Color.dark_grey(), 'grey'],
-            19: [Color.light_grey(), 'light_grey'],
-            20: [Color.darker_grey(), 'darker_grey']
-        }
-        if len(args) > 0:
-            if args[0] == 'full':
-                await ctx.send('*Incoming!* :boom:')
-                for key, value in colors.items():
-                    color_num = str(hash(value[0]))
-                    embed = Embed(color=value[0])
-                    embed.add_field(name=value[1], value=color_num)
-                    await ctx.author.send(embed=embed)
-            return
-        embed = Embed(colors=Color.blue())
+    @commands.group()
+    async def colors(self, ctx):
+        colors = RichEmbed.color_dict
+        if ctx.invoked_subcommand is None:
+            embed = Embed(colors=Color.blue())
+            for key, value in colors.items():
+                color_num = str(hash(value[0]))
+                embed.add_field(name=value[1], value=color_num)
+            embed.add_field(name='u2oob', value='Try ``colors full`` for embed examples.\n'
+                                                '**WARNING**: This will blow up your DMs!')
+            await ctx.author.send(embed=embed)
+
+    @colors.group()
+    async def full(self, ctx):
+        colors = RichEmbed.color_dict
+        await ctx.send('*Incoming!* :boom:')
+
         for key, value in colors.items():
             color_num = str(hash(value[0]))
+            embed = Embed(color=value[0])
             embed.add_field(name=value[1], value=color_num)
-        embed.add_field(name='u2oob', value='Try ``colors full`` for embed examples.\n'
-                                            '**WARNING**: This will blow up your DMs!')
-        await ctx.author.send(embed=embed)
+            await ctx.author.send(embed=embed)
 
-    @commands.command()
-    async def richembed(self, ctx, *args):
-        try:
-            if args[0] == 'get':
-                msg = await ctx.get_message(id=args[1])
-                e_msg = msg.embeds[0].to_dict()
-                await ctx.send(e_msg)
-                return
-        except IndexError as e:
-            print(e)
-            await ctx.send('An error occurred. Please review your command and try again.')
-            return
-        try:
-            if args[0] == 'pasta':
-                msg = ' '.join(args[1:])
+    @commands.group()
+    async def richembed(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send('Invoke `richembed` with `get`, `pasta`, or `example`.')
 
-                # format for json
-                msg = msg.replace('True', 'true')
-                msg = msg.replace('False', 'false')
-                msg = msg.replace('None', 'null')
-                d = json.loads(msg.replace('\'', ''))
+    @richembed.group()
+    async def get(self, ctx, *args):
+        msg = await ctx.get_message(id=args[0])
+        e_msg = msg.embeds[0].to_dict()
+        await ctx.send(e_msg)
 
-                embed = Embed.from_data(d)
-                await ctx.send(embed=embed)
-                return
-        except Exception as e:
-            print(e)
-            await ctx.send('An error occurred. Please review your command and try again.')
-            raise
-        try:
-            example = ['example', 'ex']
-            if args[0] in example:
-                embed = Embed(
-                    title='This is the embed\'s title.',
-                    description='This is the embed description',
-                    color=Color.blue()
-                )
-                embed.set_footer(text='This is the footer')
-                embed.set_image(
-                    url='http://vignette1.wikia.nocookie.net/pantheonthelegends/images/d/d2/Artemis.png/revision/latest?cb=20130604072452')
-                embed.set_thumbnail(
-                    url='http://vignette1.wikia.nocookie.net/pantheonthelegends/images/d/d2/Artemis.png/revision/latest?cb=20130604072452')
-                embed.set_author(name='Author',
-                                 icon_url='http://vignette1.wikia.nocookie.net/pantheonthelegends/images/d/d2/Artemis.png/revision/latest?cb=20130604072452')
-                embed.add_field(name='This is the first field name.', value='This is the first field name\'s value.', inline=False)
-                embed.add_field(name='This is the second field name.', value='This is the second field name\'s value.', inline=False)
-                await ctx.send(embed=embed)
-                await ctx.send(embed.to_dict())
-        except IndexError as e:
-            print(e)
-            await ctx.send('An error occurred. Please review your command and try again.')
-            return
+    @richembed.group()
+    async def pasta(self, ctx, *args):
+        msg = ' '.join(args)
+        # format for json
+        msg = msg.replace('True', 'true')
+        msg = msg.replace('False', 'false')
+        msg = msg.replace('None', 'null')
+        msg = msg.replace('\'', '"')
+        d = json.loads(msg)
+        embed = Embed.from_data(d)
+        await ctx.send(embed=embed)
+
+    @richembed.group()
+    async def example(self, ctx):
+        embed = Embed(
+            title='This is the embed title.',
+            description='This is the embed description',
+            color=Color.blue()
+        )
+        embed.set_footer(
+            text='This is the footer.'
+        )
+        embed.set_image(
+            url='http://icons.iconarchive.com/icons/papirus-team/papirus-apps/512/discord-icon.png')
+        embed.set_thumbnail(
+            url='http://icons.iconarchive.com/icons/papirus-team/papirus-apps/512/discord-icon.png')
+        embed.set_author(
+            name='Author',
+            icon_url='http://icons.iconarchive.com/icons/papirus-team/papirus-apps/512/discord-icon.png')
+        embed.add_field(
+            name='This is the first field name.',
+            value='This is the first field name value.',
+            inline=False)
+        embed.add_field(
+            name='This is the second field name.',
+            value='This is the second field name value.',
+            inline=False)
+        embed_dict = embed.to_dict()
+        await ctx.send(embed=embed)
+        await ctx.send(embed_dict)
 
     async def on_message(self, message):
         embed = Embed(color=Color.blue())
