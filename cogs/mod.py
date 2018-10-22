9f087f2da88455919f73915fdab816bfaf7a9839
Hatchet Jackk <rob.marshall06@gmail.com>
2018-10-22 09:28:59 -0400

    removed embeddisplay and improved test

--- cogs/mod.py
+++ cogs/mod.py
@@ -2,6 +2,7 @@
 Mod module for administrator commands
 """
 import discord
+from datetime import datetime
 import json
 from discord.ext import commands
 
@@ -27,46 +28,48 @@ class Mod:
             await ctx.send("You do not have permission to do that.")
             return
 
-    # owner command
     @commands.command()
+    @commands.has_role('mod')
     async def test(self, ctx):
         author = ctx.author
-        channel = ctx.channel
         guild = ctx.guild
+        channels = [channel.name for channel in guild.channels]
+        roles = [role.name for role in guild.roles if role.name != '@everyone']
 
-        if author.id == '193416878717140992':
-            print('author', author)
-            print('channel', channel)
-            print('guild', guild)
-            print('member', discord.Object(id='496516622903803905'))
-            # for role in guild.roles:
-            #     print('role', role)
-            print('roles', discord.utils.get(guild.roles, name='Sparkle!, Sparkle!'))
-            # print('role', discord.utils.get(name='Sparkle!'))
-            # print('role', guild.roles(name='Sparkle!'))
-            print('dchannel', str(channel))
+        await ctx.send('Printing test\n'
+                       'Author: {}\n'
+                       'Guild: {}\n'
+                       'Channels: {}\n'
+                       'Roles: {}\n'
+                       'Time: {}'.format(author.name,
+                                         guild,
+                                         channels,
+                                         roles,
+                                         datetime.utcnow())
+                       )
 
     # owner command
-    # @commands.command()
-    # async def load(self, ctx, extension):
-    #     author = ctx.author
-    #
-    #     if author.id == "193416878717140992":
-    #         try:
-    #             self.client.load_extension(extension)
-    #         except Exception as error:
-    #             print('{0} cannot be loaded [{1}]'.format(extension, error))
-    #             return
-    #     else:
-    #         await ctx.send("You do not have permission to do that.")
-    #         return
-    #     spam = ['botspam']
-    #     channels = ctx.get_all_channels()
-    #     for channel in channels:
-    #         ch = channel.name
-    #         if ch in spam:
-    #             # pass
-    #             await ctx.send(ctx.channel.message, '{0} loaded.'.format(extension))
+    @commands.command()
+    @commands.is_owner()
+    async def load(self, ctx, extension):
+        author = ctx.author
+
+        if author.id == "193416878717140992":
+            try:
+                self.client.load_extension(extension)
+            except Exception as error:
+                print('{0} cannot be loaded [{1}]'.format(extension, error))
+                return
+        else:
+            await ctx.send("You do not have permission to do that.")
+            return
+        spam = ['botspam']
+        channels = ctx.get_all_channels()
+        for channel in channels:
+            ch = channel.name
+            if ch in spam:
+                # pass
+                await ctx.send(ctx.channel.message, '{0} loaded.'.format(extension))
 
     # owner command
     @commands.command()
@@ -90,32 +93,8 @@ class Mod:
                 # pass
                 await ctx.send(ctx.channel.message, '{0} unloaded.'.format(extension))
 
-    # mod command
     @commands.command()
-    async def displayembed(self, ctx):
-        author = ctx.author
-
-        mod = '495187511698784257'
-        if mod or "193416878717140992" in [role.id for role in author.roles]:
-            # hex colors
-            # int(767,a76, 16)
-            embed = discord.Embed(
-                title="Title",
-                description="Description",
-                color=discord.Color.blue()
-            )
-            embed.set_footer(text="Footer")
-            embed.set_image(url="http://promoboxx.com/compare/images/broken_robot.png")
-            embed.set_thumbnail(url="http://promoboxx.com/compare/images/broken_robot.png")
-            embed.set_author(name="Author Name", icon_url="http://promoboxx.com/compare/images/broken_robot.png")
-            embed.add_field(name="Field Name", value="Field Value", inline=False)
-            await ctx.send(embed=embed)
-        else:
-            await ctx.send("You do not have permission to do that.")
-
-    # owner command
-    @commands.command()
-    @commands.has_role('mod')
+    @commands.is_owner()
     async def prefix(self, ctx, prefix: str):
         # change the prefix for the guild
         guild = ctx.guild
