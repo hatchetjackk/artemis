5da107310fe38c93f3f1c472bb84422447d5de76
Hatchet Jackk <rob.marshall06@gmail.com>
2018-10-26 12:53:32 -0400

    removed unused functions

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,48 +1,10 @@
 import json
-from datetime import datetime
 from discord.ext import commands
 
 
 class Mod:
     def __init__(self, client):
         self.client = client
-
-    # owner command
-    @commands.command()
-    async def log(self, ctx):
-        author = ctx.author
-        channel = ctx.channel
-
-        if author.id == "193416878717140992":
-            counter = 0
-            tmp = await ctx.send("Calculating messages...")
-            async for log in self.client.logs_from(channel, limit=100):
-                if log.author == author:
-                    counter += 1
-            await self.client.edit_message(tmp, "You have {0} messages.".format(counter))
-        else:
-            await ctx.send("You do not have permission to do that.")
-            return
-
-    @commands.command()
-    @commands.has_role('mod')
-    async def test(self, ctx):
-        author = ctx.author
-        guild = ctx.guild
-        channels = [channel.name for channel in guild.channels]
-        roles = [role.name for role in guild.roles if role.name != '@everyone']
-
-        await ctx.send('Printing test\n'
-                       'Author: {}\n'
-                       'Guild: {}\n'
-                       'Channels: {}\n'
-                       'Roles: {}\n'
-                       'Time: {}'.format(author.name,
-                                         guild,
-                                         channels,
-                                         roles,
-                                         datetime.utcnow())
-                       )
 
     # owner command
     @commands.command()
