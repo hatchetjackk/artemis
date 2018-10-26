5e1b4c2d83fe082eab73ac864e2424c5b49cdd55
Hatchet Jackk <rob.marshall06@gmail.com>
2018-10-26 12:45:11 -0400

    modified for knights

--- cogs/fun.py
+++ cogs/fun.py
@@ -82,6 +82,59 @@ class Fun:
             reddit_search = 'https://reddit.com/' + msg
             await channel.send(reddit_search)
 
+    @commands.command()
+    async def rps(self, ctx, *args):
+        rps = ['rock', 'paper', 'scissors']
+        lose = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'}
+        win = {'paper': 'rock', 'rock': 'scissors', 'scissors': 'paper'}
+
+        # check for valid entry
+        if len(args) < 1:
+            await ctx.send('You didn\'t say what you picked!')
+            return
+        if len(args) > 1:
+            await ctx.send(
+                "It's Rock, Paper, Scissors not Rock, Paper, \"*Whateverthehellyouwant*.\" :joy:")
+            return
+        if args[0] not in rps:
+            await ctx.send('That\'s not a valid choice.')
+            return
+
+        bot_choice = random.choice(rps)
+        user_choice = args[0]
+        # check choice against bot
+        if bot_choice == user_choice:
+            await ctx.send('Artemis chose {0}! It\'s a tie!'.format(bot_choice))
+            print('{0} played rock, paper, scissors and tied with Artemis.'.format(ctx.author.name))
+        if user_choice == lose.get(bot_choice):
+            await ctx.send('Artemis chose {0}! You lost!'.format(bot_choice))
+            print('{0} played rock, paper, scissors and lost to Artemis.'.format(ctx.author.name))
+        if user_choice == win.get(bot_choice):
+            await ctx.send('Artemis chose {0}! You win!'.format(bot_choice))
+            print('{0} played rock, paper, scissors and beat Artemis!'.format(ctx.author.name))
+
+    @commands.command(aliases=['yt'])
+    async def youtube(self, ctx, *args):
+        query_string = urllib.parse.urlencode({"search_query": ' '.join(args)})
+        html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+        search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+
+        await ctx.send("http://www.youtube.com/watch?v=" + search_results[0])
+        print('{0} searched Youtube for "{1}".'.format(ctx.author.name, ' '.join(args)))
+
+    @commands.command()
+    async def card(self, ctx):
+        """ draw a random card from a deck """
+        card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Ace', 'King', 'Queen', 'Jack']
+        style = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+
+        rcard = random.choice(card)
+        rstyle = random.choice(style)
+
+        fmt = 'You drew the {0} of {1}!'
+        await ctx.send(fmt.format(rcard, rstyle))
+        print('{0} drew the {1} of {2}.'.format(ctx.author.name, rcard, rstyle))
+
 
 def setup(client):
     client.add_cog(Fun(client))
