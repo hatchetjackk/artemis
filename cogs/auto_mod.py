b269d0d8b2330de724d87a48d0e3500f6be8fb63
Hatchet Jackk <rob.marshall06@gmail.com>
2018-11-21 11:24:08 -0500

    update auto_mod to db

--- cogs/auto_mod.py
+++ cogs/auto_mod.py
@@ -1,5 +1,5 @@
 import discord
-import json
+from artemis import load_db
 from discord.ext import commands
 
 
@@ -8,75 +8,62 @@ class Automod:
         self.client = client
         self.auto_mod_blacklist = ['Knights of Karma', 'Aurora Corporation']
 
+    @commands.command()
+    async def roles(self, ctx):
+        for role in ctx.guild.roles:
+            await ctx.send(role)
+
     @commands.group()
-    @commands.has_any_role('Moderator', 'mod', 'admin', 'Admin')
+    # @commands.has_any_role('Moderator', 'mod', 'admin', 'Admin')
     async def modrole(self, ctx):
         if ctx.invoked_subcommand is None:
             await ctx.send('Invoke `modrole` with `add` or `remove`.')
 
     @modrole.group()
-    async def add(self, ctx, role: str):
-        guild = ctx.guild
-        if guild.name in self.auto_mod_blacklist:
+    async def set(self, ctx, *, role: str):
+        if ctx.guild.name in self.auto_mod_blacklist:
             return
-        gid = str(guild.id)
         role = discord.utils.get(ctx.guild.roles, name=role)
-        data = await self.load_guilds()
-        data[gid]['mod_roles'].append(role)
-        await self.dump_guilds(data)
+        conn = await load_db()
+        c = conn.cursor()
+        with conn:
+            c.execute("UPDATE guilds SET mod_role = (?) WHERE id = (?)", (role.name, ctx.guild.id))
         await ctx.send('{} added to the moderator list.'.format(role))
 
     @modrole.group()
-    async def remove(self, ctx, role: str):
-        guild = ctx.guild
-        if guild.name in self.auto_mod_blacklist:
+    async def remove(self, ctx):
+        if ctx.guild.name in self.auto_mod_blacklist:
             return
-        gid = str(guild.id)
-        role = discord.utils.get(ctx.guild.roles, name=role)
-        data = await self.load_guilds()
-        data[gid]['mod_roles'].remove(role)
-        await self.dump_guilds(data)
-        await ctx.send('{} removed from the moderator list.'.format(role))
+        conn = await load_db()
+        c = conn.cursor()
+        with conn:
+            c.execute("UPDATE guilds SET mod_role = (?) WHERE id = (?)", (None, ctx.guild.id))
+        await ctx.send('The modrole was removed.')
 
     @commands.group()
-    @commands.has_any_role('Moderator', 'mod')
+    # @commands.has_any_role('Moderator', 'mod')
     async def autorole(self, ctx):
         if ctx.invoked_subcommand is None:
             await ctx.send('Invoke `autorole` with `add` or `remove`.')
 
     @autorole.group(aliases=['add'])
-    async def set(self, ctx, role):
-        data = await self.load_guilds()
-        guild = ctx.guild
-        gid = str(guild.id)
-        author = ctx.author
-        role = discord.utils.get(guild.roles, name=role)
-        try:
-            if role in guild.roles:
-                data[gid]['auto_role'] = role.id
-                await self.dump_guilds(data)
-                await ctx.send('The Autorole is now set to `{}`'.format(role))
-                msg = '{0} set {1}\'s autorole to *{2}*.'.format(author.name, guild, role)
-                await self.spam(ctx, msg)
-            else:
-                await ctx.ctx.send('{} is not a valid role.'.format(role))
-        except Exception as e:
-            print(e)
+    async def set(self, ctx, *, role: str):
+        role = discord.utils.get(ctx.guild.roles, name=role)
+        conn = await load_db()
+        c = conn.cursor()
+        with conn:
+            c.execute("UPDATE guilds SET autorole = (?) WHERE id = (?)", (role.id, ctx.guild.id))
+        msg = '{0} set {1}\'s autorole to *{2}*.'.format(ctx.author.name, ctx.guild.name, role.name)
+        await self.spam(ctx, msg)
 
     @autorole.group()
     async def remove(self, ctx):
-        data = await self.load_guilds()
-        guild = ctx.guild
-        author = ctx.author
-        try:
-            data[guild.id]['auto_role'] = None
-            await self.dump_guilds(data)
-            await ctx.send('The Autorole has been cleared.')
-            msg = '{0} cleared {1}\'s autorole.'.format(author.name, guild)
-            await self.spam(ctx, msg)
-        except Exception as e:
-            print(e)
-            raise
+        conn = await load_db()
+        c = conn.cursor()
+        with conn:
+            c.execute("UPDATE guilds SET autorole = (?) WHERE id = (?)", (None, ctx.guild.id))
+        msg = '{0} cleared {1}\'s autorole.'.format(ctx.author.name, ctx.guild.name)
+        await self.spam(ctx, msg)
 
     # @commands.command(aliases=['spam'])
     # @commands.has_any_role('mod', 'Moderator')
@@ -108,25 +95,20 @@ class Automod:
             return
         await ctx.channel.purge(limit=amount+1)
 
-    async def on_member_join(self, member):
+    @staticmethod
+    async def on_member_join(member):
         # when a member joins, give them an autorole if it exists
-        data = await self.load_guilds()
-        guild = member.guild
-        gid = str(guild.id)
-        role = data[gid]['auto_role']
-
-        if role is not None:
-            role = discord.utils.get(
-                member.guild.roles,
-                id=data[gid]['auto_role']
-            )
-            await  member.add_roles(role)
-            # await self.client.add_roles(member, role)
-        await self.create_user(member)
-
-        if gid in data:
-            if data[gid]['spam'] is not None:
-                msg1 = '{0.name} joined {1}.'.format(member, guild)
+        conn = await load_db()
+        c = conn.cursor()
+        with conn:
+            c.execute("SELECT autorole FROM guilds WHERE id = (?)", (member.guild.id,))
+            role = member.guild.get_role(c.fetchone()[0])
+            if role is not None:
+                await member.add_roles(role)
+            c.execute("SELECT spam FROM guilds WHERE id = (?)", (member.guild.id,))
+            spam = c.fetchone()[0]
+            if spam is not None:
+                msg1 = '{0.name} joined {1}.'.format(member, member.guild)
                 msg2 = '{0} was assigned the autorole {1}'.format(member.name, role)
                 embed = discord.Embed(color=discord.Color.blue())
                 embed.set_thumbnail(url=member.avatar_url)
@@ -140,27 +122,27 @@ class Automod:
                     value=msg2,
                     inline=False
                 )
-                channel = self.client.get_channel(data[gid]['spam'])
+                channel = member.guild.get_channel(spam)
                 await channel.send(embed=embed)
         channel = discord.utils.get(member.guild.channels, name='general')
-        await channel.send('Welcome to {}, {}!'.format(guild.name, member.name))
+        await channel.send('Welcome to {}, {}!'.format(member.guild.name, member.name))
 
-    async def on_member_remove(self, member):
-        # when a member joins, give them an autorole if it exists
-        data = await self.load_guilds()
-        guild = member.guild
-        gid = str(guild.id)
-
-        if gid in data:
-            if data[gid]['spam'] is not None:
-                msg = '{0.name} has left {1}.'.format(member, guild)
+    @staticmethod
+    async def on_member_remove(member):
+        conn = await load_db()
+        c = conn.cursor()
+        with conn:
+            c.execute("SELECT spam FROM guilds WHERE id = (?)", (member.guild.id,))
+            spam = c.fetchone()[0]
+            if spam is not None:
+                msg = '{0.name} has left {1}.'.format(member, member.guild)
                 embed = discord.Embed(color=discord.Color.blue())
                 embed.add_field(
                     name='Alert',
                     value=msg,
                     inline=False
                 )
-                channel = self.client.get_channel(data[gid]['spam'])
+                channel = member.guild.get_channel(spam)
                 await channel.send(embed=embed)
 
     async def on_message_edit(self, before, after):
@@ -169,9 +151,6 @@ class Automod:
         if before.author.bot:
             return
         try:
-            guild = before.guild
-            gid = str(guild.id)
-
             embed = discord.Embed(
                 title='{0} edited a message'.format(after.author.name),
                 description='in channel {0.mention}.'.format(after.channel),
@@ -189,83 +168,55 @@ class Automod:
                 inline=False
             )
 
-            data = await self.load_guilds()
-            channel = self.client.get_channel(data[gid]['spam'])
-            if channel is None:
-                return
-            await channel.send(embed=embed)
+            conn = await load_db()
+            c = conn.cursor()
+            with conn:
+                c.execute("SELECT spam FROM guilds WHERE id = (?)", (before.guild.id,))
+                if c.fetchone()[0] is None:
+                    return
+                spam = before.guild.get_channel(c.fetchone()[0])
+            await spam.send(embed=embed)
         except Exception as e:
             print(e)
 
-    async def on_message_delete(self, message):
-        guild = message.guild
-        gid = str(guild.id)
-
+    @staticmethod
+    async def on_message_delete(message):
         if message.author.bot:
             return
         msg = '{0.author.name}\'s message was deleted:\n' \
               '**Channel**: {0.channel.mention}\n' \
               '**Content**: {0.content}'
 
-        data = await self.load_guilds()
-        if gid in data:
-            if data[gid]['spam'] is not None:
+        conn = await load_db()
+        c = conn.cursor()
+        with conn:
+            c.execute("SELECT spam FROM guilds WHERE id = (?)", (message.guild.id,))
+            spam = c.fetchone()[0]
+            if spam is not None:
                 embed = discord.Embed(color=discord.Color.blue())
                 embed.add_field(
                     name='Alert',
                     value=msg.format(message)
                 )
-                channel = self.client.get_channel(data[gid]['spam'])
+                channel = message.guild.get_channel(spam)
                 if channel is None:
                     return
                 await channel.send(embed=embed)
 
     @staticmethod
-    async def create_user(member):
-        guild = member.guild
-        gid = str(guild.id)
-        mid = str(member.id)
-
-        with open('files/users.json', 'r') as f:
-            data_users = json.load(f)
-
-        if mid not in data_users:
-            data_users[member.id] = {
-                'username': member.name,
-                'guild': {},
-                'karma': 0,
-                'karma_cooldown': 0
-            }
-        if gid not in data_users[mid]['guild']:
-            data_users[mid]['guild'].update({gid: guild.name})
-
-        with open('files/users.json', 'w') as f:
-            json.dump(data_users, f, indent=2)
-
-    @staticmethod
-    async def load_guilds():
-        with open('files/guilds.json') as f:
-            data = json.load(f)
-        return data
-
-    @staticmethod
-    async def dump_guilds(data):
-        with open('files/guilds.json', 'w') as f:
-            json.dump(data, f, indent=2)
-
-    async def spam(self, ctx, message):
-        guild = ctx.guild
-        gid = str(guild.id)
-
-        data = await self.load_guilds()
-        if gid in data:
-            if data[gid]['spam'] is not None:
+    async def spam(ctx, message):
+        conn = await load_db()
+        c = conn.cursor()
+        with conn:
+            c.execute("SELECT spam FROM guilds WHERE id = (?)", (ctx.guild.id,))
+            spam = c.fetchone()[0]
+            if spam is not None:
                 embed = discord.Embed(color=discord.Color.blue())
                 embed.add_field(
                     name='Alert',
                     value=message
                 )
-                channel = self.client.get_channel(data[gid]['spam'])
+                channel = ctx.guild.get_channel(spam)
                 await channel.send(embed=embed)
 
     @autorole.error
