16407b9b9eb419aa4f7234bee1cea6ea2e1d260b
Hatchet Jackk <rob.marshall06@gmail.com>
2018-11-21 11:24:21 -0500

    update karma to db

--- cogs/karma.py
+++ cogs/karma.py
@@ -11,7 +11,7 @@ import discord
 import random
 import time
 from collections import OrderedDict
-from artemis import load_json, dump_json
+from artemis import load_json, dump_json, load_db
 from discord.ext import commands
 
 
@@ -25,38 +25,36 @@ class Karma:
         if ctx.guild.name in self.karma_blacklist:
             return
         member_check = ' '.join(args)
-        aid = str(ctx.author.id)
-        users = await load_json('users')
-
-        if member_check == '':
-            if 'karma' not in users[aid]:
-                users[aid]['karma'] = 0
-            points = users[aid]['karma']
-            await ctx.send('You have {0} karma.'.format(points))
-        else:
-            if len(member_check) < 3:
-                await ctx.send('Please search using 3 or more characters.')
-                return
-            target_member = ''
-            for member in ctx.guild.members:
-                member_name = member.name.lower()
-                if member.nick is not None:
-                    member_name = member.nick.lower()
-                if member.mention in member_check:
-                    target_member = member
-                else:
-                    pattern = re.compile(r'' + re.escape(member_check))
-                    matches = pattern.findall(member_name)
-                    for _ in matches:
+        conn = await load_db()
+        c = conn.cursor()
+        with conn:
+            if member_check == '':
+                c.execute("SELECT karma FROM members WHERE id = (:id)", {'id': ctx.author.id})
+                points = c.fetchone()[0]
+                await ctx.send('You have {0} karma.'.format(points))
+            else:
+                if len(member_check) < 3:
+                    await ctx.send('Please search using 3 or more characters.')
+                    return
+                target_member = ''
+                for member in ctx.guild.members:
+                    member_name = member.name.lower()
+                    if member.nick is not None:
+                        member_name = member.nick.lower()
+                    if member.mention in member_check:
                         target_member = member
-            if 'karma' not in users[str(target_member.id)]:
-                users[str(target_member.id)]['karma'] = 0
-                await dump_json('users', users)
-            target_member_name = target_member.name
-            if target_member.nick is not None:
-                target_member_name = target_member.nick
-            msg = '{0} has {1} karma.'.format(target_member_name, users[str(target_member.id)]['karma'])
-            await ctx.send(msg)
+                    else:
+                        pattern = re.compile(r'' + re.escape(member_check))
+                        matches = pattern.findall(member_name)
+                        for _ in matches:
+                            target_member = member
+                c.execute("SELECT karma FROM members WHERE id = (:id)", {'id': target_member.id})
+                points = c.fetchone()[0]
+                target_member_name = target_member.name
+                if target_member.nick is not None:
+                    target_member_name = target_member.nick
+                msg = '{0} has {1} karma.'.format(target_member_name, points)
+                await ctx.send(msg)
 
     @commands.command()
     async def leaderboard(self, ctx):
@@ -94,8 +92,11 @@ class Karma:
         if author.id == self.client.user.id:
             return
 
+        conn = await load_db()
+        c = conn.cursor()
+
         karma_responses = await load_json('status')
-        data = await load_json('users')
+        # data = await load_json('users')
 
         keywords = ['thanks', 'thank', 'gracias', 'kudos', 'thx', 'appreciate', 'cheers']
         msg = [word.lower() for word in message.content.split() if len(word) >= 3]
@@ -119,20 +120,20 @@ class Karma:
                             if member not in thanked_members:
                                 thanked_members.append(member)
         for member in thanked_members:
-            mid = str(member.id)
+            # mid = str(member.id)
             # format member name
+            member_name = member.name
             if member.nick is not None:
                 member_name = member.nick
-            else:
-                member_name = member.name
-
             if len(karma_key) > 0:
+                print(1)
                 # check karma cool down
-                if 'last_karma_give' not in data[str(author.id)]:
+                c.execute("SELECT * FROM members WHERE id = (:id)", {'id': message.author.id})
+                member_id, membername, points, last_karma_given = c.fetchone()
+                if last_karma_given is None:
                     pass
                 else:
-                    last_karma_give = data[str(author.id)]['last_karma_give']
-                    remaining_time = int(time.time() - last_karma_give)
+                    remaining_time = int(time.time() - last_karma_given)
                     time_limit = 60 * 3
                     if remaining_time < time_limit:
                         msg = 'You must wait {0} seconds to give karma again.'.format(time_limit - remaining_time)
@@ -147,15 +148,17 @@ class Karma:
                     await message.channel.send(msg)
                 # if karma is going to a user and not artemis or the author
                 else:
-                    if 'karma' not in data[mid]:
-                        data[mid]['karma'] = 0
-                    data[mid]['karma'] += 1
-                    data[str(author.id)]['last_karma_give'] = time.time()
-                    await dump_json('users', data)
-                    msg = random.choice(karma_responses['karma_responses']['good_response']).format(member_name)
-                    await message.channel.send(msg)
-                    print("{0} received a karma point from {1}".format(member_name, message.author.name))
-
+                    with conn:
+                        c.execute("SELECT * FROM members WHERE id = (:id)", {'id': member.id})
+                        member_id, membername, points, last_karma_given = c.fetchone()
+                        last_karma_given = int(time.time())
+                        points += 1
+                        c.execute("UPDATE members SET karma = (:karma), last_karma_given = (:last_karma_given) WHERE id = (:id)",
+                                  {'karma': points, 'last_karma_given': last_karma_given, 'id': member.id})
+                        msg = random.choice(karma_responses['karma_responses']['good_response']).format(member_name)
+                        await message.channel.send(msg)
+                        print("{0} received a karma point from {1}".format(member_name, message.author.name))
+                        return
 
 
 def setup(client):
