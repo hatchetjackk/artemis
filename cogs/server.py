851e5d76ec633e3eddcf000ed3375d497a7d7be8
Rob Marshall <rob.marshall06@gmail.com>
2019-04-04 07:52:49 -0400

    feat: gave !roll a modifier

--- cogs/server.py
+++ cogs/server.py
@@ -1,4 +1,5 @@
 import sqlite3
+import sqlalchemy
 
 from discord.ext import commands
 
@@ -9,8 +10,22 @@ class Servers(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @commands.command(aliases=['guildinfo'])
+    async def server_data(self, ctx):
+        """
+        Return data about the current guild
+        :param ctx:
+        :return:
+        """
+        pass
+
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
+        """
+        Notify that artemis has joined a server and initiate the configuration. Add the guild to Artemis' database.
+        :param guild: The guild that artemis has joined
+        :return:
+        """
         for channel in guild.channels:
             if channel.name.lower() == 'general':
                 await utilities.single_embed(
@@ -22,9 +37,15 @@ class Servers(commands.Cog):
         conn, c = await utilities.load_db()
         with conn:
             try:
-                c.execute("INSERT INTO guilds VALUES (:id, :guild, :mod_role, :autorole, :prefix, :spam, :thumbnail)",
-                          {'id': guild.id, 'guild': guild.name, 'mod_role': None,
-                           'autorole': None, 'prefix': '!', 'spam': None, 'thumbnail': None})
+                c.execute("INSERT INTO guilds VALUES (:gid, :guild, :modrole, :autorole, :prefix, :spam, :thumbnail)", {
+                    'gid': guild.id,
+                    'guild': guild.name,
+                    'modrole': None,
+                    'autorole': None,
+                    'prefix': '!',
+                    'spam': None,
+                    'thumbnail': None
+                })
                 print(f'Artemis has joined the {guild.name} guild!')
             except sqlite3.DatabaseError as e:
                 print(f'An error occurred when Artemis joined {guild.name}: {e}')
@@ -35,18 +56,29 @@ class Servers(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_update(self, before, after):
+        """
+        Update guild information in the database
+        :param before: The data before the guild updated
+        :param after: The data after the guild updated
+        :return:
+        """
         conn, c = await utilities.load_db()
         if before.name != after.name:
+            c.execute("SELECT modrole, autorole, prefix, spam, thumbnail FROM guilds WHERE gid = (:gid)",
+                      {'gid': before.id})
+            modrole, autorole, prefix, spam, thumbnail = c.fetchall()[0]
             with conn:
-                c.execute("SELECT mod_role, autorole, prefix, spam, thumbnail FROM guilds WHERE id = (:id)",
-                          {'id': before.id})
-                mod_role, autorole, prefix, spam, thumbnail = c.fetchall()[0]
                 try:
                     c.execute(
-                        "REPLACE INTO guilds VALUES (:id, :guild, :mod_role, :autorole, :prefix, :spam, :thumbnail)",
-                        {'id': before.id, 'guild': after.name, 'mod_role': mod_role, 'autorole': autorole,
-                         'prefix': prefix, 'spam': spam, 'thumbnail': thumbnail}
-                    )
+                        "REPLACE INTO guilds VALUES (:gid, :guild, :modrole, :autorole, :prefix, :spam, :thumbnail)", {
+                            'gid': before.id,
+                            'guild': after.name,
+                            'modrole': modrole,
+                            'autorole': autorole,
+                            'prefix': prefix,
+                            'spam': spam,
+                            'thumbnail': thumbnail
+                        })
                     print(f'The {before.name} guild changed its name to {after.name}.')
                 except sqlite3.Error as e:
                     print(f'An error occurred on guild update: {e}')
@@ -54,17 +86,22 @@ class Servers(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def clean_database(self, ctx):
+        """
+        Completely wipe the database. This cannot be undone!
+        :param ctx:
+        :return:
+        """
         conn, c = await utilities.load_db()
         with conn:
-            c.execute("SELECT member_id, member_name FROM guild_members WHERE id = (:id)", {'id': ctx.guild.id})
+            c.execute("SELECT uid, user FROM guild_members WHERE gid = (:gid)", {'gid': ctx.guild.id})
             db_member_ids = c.fetchall()
             current_guild_ids = [member.id for member in ctx.guild.members]
             for user in db_member_ids:
                 uid, name = user
                 if uid not in current_guild_ids:
                     try:
-                        c.execute("DELETE FROM guild_members WHERE id = (:id) and member_id = (:member_id)",
-                                  {'id': ctx.guild.id, 'member_id': user[0]})
+                        c.execute("DELETE FROM guild_members WHERE gid = (:gid) and uid = (:uid)",
+                                  {'gid': ctx.guild.id, 'uid': user[0]})
                         print(f'{name} ({uid}) cleaned from {ctx.guild.name}.')
                     except Exception as e:
                         print(f'An error occurred when attempting to clean the database: {e}')
